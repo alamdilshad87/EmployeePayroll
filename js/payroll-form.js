10f60fc6002ec44e2d61659ac538537abdcfd818
alamdilshad87 <dilshadand@gmail.com>
2026-01-16 21:26:46 +0530

    feat(UC 10): Data Object populate the Employee Payroll Data Object

--- js/payroll-form.js
+++ js/payroll-form.js
@@ -10,3 +10,62 @@ window.addEventListener("DOMContentLoaded", () => {
         salaryOutput.textContent = salarySlider.value;
     });
 });
+
+// UC-9: Handle Form Submit
+
+window.addEventListener("DOMContentLoaded", () => {
+    const form = document.querySelector(".form");
+    form.addEventListener("submit", handleFormSubmit);
+});
+
+function handleFormSubmit(event) {
+    event.preventDefault();
+
+    const employeePayrollData = createEmployeePayrollData();
+    console.log("Employee Payroll Object:", employeePayrollData);
+}
+
+function createEmployeePayrollData() {
+    const name = document.querySelector("#name").value;
+
+    const profile = document.querySelector(
+        'input[name="profile"]:checked'
+    ).value;
+
+    const gender = document.querySelector(
+        'input[name="gender"]:checked'
+    ).value;
+
+    const department = getSelectedDepartments();
+
+    const salary = document.querySelector("#salary").value;
+
+    const day = document.querySelector("#day").value;
+    const month = document.querySelector("#month").value;
+    const year = document.querySelector("#year").value;
+
+    const notes = document.querySelector("#notes").value;
+
+    return {
+        name,
+        profile,
+        gender,
+        department,
+        salary,
+        startDate: `${day} ${month} ${year}`,
+        notes
+    };
+}
+
+function getSelectedDepartments() {
+    const departmentNodes = document.querySelectorAll(
+        'input[type="checkbox"]:checked'
+    );
+
+    let departments = [];
+    departmentNodes.forEach(node => {
+        departments.push(node.value);
+    });
+
+    return departments;
+}
